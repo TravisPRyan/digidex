bcabead0f9989a0d000136fd302e880e69b35190
TravisPRyan <TravisPRyan@protonmail.com>
2019-11-21 17:05:43 -0500

    v4 User/Validation

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Push 3: Implement global variables via config. Connect to DB.
+Push 4: Create User model. Implement validation (express-validator).
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+// https://express-validator.github.io/docs/index.html
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Only valid email can be used.").isEmail(),
+    check("password", "Enter a password of 8 or more characters").isLength({
+      min: 8
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
