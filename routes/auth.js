c3c1f24dedb1223fa95e7c4538d2566e2f96f438
TravisPRyan <TravisPRyan@protonmail.com>
2019-11-23 15:41:19 -0500

    v8 Auth Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,21 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 // https://express-validator.github.io/docs/index.html
 const { check, validationResult } = require("express-validator");
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/auth
@@ -21,7 +28,7 @@ router.post(
   "/",
   [
     check("email", "Only valid email can be used.").isEmail(),
-    check("password", "Enter a password of 8 or more characters").exists()
+    check("password", "Invalid password ").exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
