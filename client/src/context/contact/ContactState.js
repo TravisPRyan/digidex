4ea62d21a1e994de4657ea2f74e80c8a951e1fc5
TravisPRyan <TravisPRyan@protonmail.com>
2019-12-17 17:20:01 -0500

    v31 Update

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -65,6 +65,33 @@ const ContactState = props => {
     }
   };
 
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.message
+      });
+    }
+  };
+
   //Delete contact
   const deleteContact = async id => {
     try {
@@ -95,11 +122,6 @@ const ContactState = props => {
   //Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter contacts
