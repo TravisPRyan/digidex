d57cb3b15c9d850031a912510b466ebd2e41eb71
TravisPRyan <TravisPRyan@protonmail.com>
2019-12-20 13:02:38 -0500

    v32 Production Deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 // Connect DB
@@ -8,13 +9,19 @@ connectDB();
 // bodyparser middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "You have reached the digiDex API" })
-);
-
 //Route definitions
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
+
+//Serve static assests (production)
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`digiDex server started on PORT: ${PORT}`));
